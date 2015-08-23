da8223292266f86f0ee23cdda1b9c428d7d50f84
Sek Cheong <scheong@gmail.com>
2015-08-22 19:40:45 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.IO;
+using Axiom;
+
+namespace printf
+{
+	
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ Util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.IO;
+
+namespace Axiom
+{
+	public static class Util
+	{
+
+		private static StringBuilder sbprintf(string text, params object[] args)
+		{
+			if (string.IsNullOrEmpty(text)) return new StringBuilder();
+			StringBuilder sb = new StringBuilder(text.Length);
+			return sb;
+		}
+
+		public static void printf(string text, params object[] args)
+		{
+
+		}
+
+		public static void sprintf(Stream s, string text, params object[] args)
+		{
+
+		}
+	}
+
+}
